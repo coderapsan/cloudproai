475de3519733e848bb87eccfac0b9f691048bfda
thedeewastamang <dtamang1324@gmail.com>
2023-11-07 20:18:28 +0545

    d

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
-import NavBar from "./Components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/Navbar";
 import HomePage from "./pages/homePage";
 import ProductsPage from "./pages/productsPage";
 import AboutPage from "./pages/aboutPage";
-import Footer from "./Components/Footer";
+import Footer from "./components/Footer";
 
 function App() {
-  return (<div>
-
- 
+  return (
     <BrowserRouter>
       <NavBar />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/products" component={ProductsPage} />
-        <Route path="/about" component={AboutPage} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-    <Footer/>
-    </div>
-
   );
 }
 
 export default App;
-
-
-
-{/* <Routes> 
-<div>
-  <NavBar />
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/products">Products</Link>
-    </li>
-    <li>
-      <Link to="/about">About</Link>
-    </li>
-  </ul>
-
-  </div>
-      </Routes> */}
